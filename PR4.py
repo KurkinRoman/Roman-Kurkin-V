eac1687106bc1d268bf6a209de40e15e94ab6ae9
Roman <98593193+KurkinRoman@users.noreply.github.com>
2022-01-29 12:40:27 +0300

    Delete PR4.py

--- PR4.py
+++
@@ -1,9 +0,0 @@
-x = input()     #Задача №2
-print(x.count(" ") + 1)
-
-word = input()                      #Задача №9
-print ("Исходная строка: " + word) 
-x = input()
-res_str = word.replace(x,' ') 
-# Удаление всех 't' 
-print("Строка после удаления всех букв " + x + ":" + res_str) 
