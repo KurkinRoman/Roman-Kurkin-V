2adbcb96412bc415389ac38f195b90f06f1ce8ee
Roman <98593193+KurkinRoman@users.noreply.github.com>
2022-01-29 12:41:16 +0300

    Add files via upload

---
+++ PR4.py
@@ -0,0 +1,8 @@
+x = input()     #Задача №2
+print(x.count(" ") + 1)
+
+word = input()                      #Задача №9
+print ("Исходная строка: " + word) 
+x = input()
+res_str = word.replace(x,' ') 
+print("Строка после удаления всех букв " + x + ":" + res_str) 
